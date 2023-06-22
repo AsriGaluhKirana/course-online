58e1949ef30f230290a9829435dbf597efc30618
asrigaluh <124650175+AsriGaluhK@users.noreply.github.com>
2023-06-22 15:37:13 +0700

    update endpoint enroll

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1@localhost:5432/courseonline?sslmode=disable'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1@localhost:5432/courseonline'
 db = SQLAlchemy(app)
 
 #table pengguna
 class Pengguna(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.String, primary_key=True, nullable=False)
     nama = db.Column(db.String)
     password = db.Column(db.String, nullable=False)
     role = db.Column(db.String, nullable=False)
@@ -21,40 +20,39 @@ def login():
     
     try:
         global user
-        user = Pengguna.query.filter_by(email=username).first_or_404()
+        user = Pengguna.query.filter_by(nama=username).first_or_404()
     except:
         return {
             'error message' : 'Hayo salah.'
         }
-        
     if user.password == password:
-        if user.tipe == 'Admin':
-            return 'Admin'
-        elif user.tipe == 'Student':
-            return 'Student'
+        if user.role == 'Admin':
+            return user
+        elif user.role == 'Student':
+            return user
     else:
         return 'Password salah!'
 
 #tabel course
 class Course(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    Nama = db.Column(db.String, nullable=False)
+    id = db.Column(db.String, primary_key=True, nullable=False)
+    nama = db.Column(db.String, nullable=False)
     deskripsi = db.Column(db.String, nullable=False)
     kategori = db.Column(db.String, nullable=False)
     student = db.relationship('Coursedata', backref='course', lazy='dynamic')
 
 # tabel course data
 class Coursedata(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("pengguna.id"), nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey("course.id"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.String, db.ForeignKey("pengguna.id"))
+    course_id = db.Column(db.String, db.ForeignKey("course.id"))
     status = db.Column(db.String, nullable=False)
 
 # tabel pre-requsite
 class Prequisite(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    course_id = db.Column(db.Integer, db.ForeignKey("course.id"))
-    prequisite_id = db.Column(db.Integer, db.ForeignKey("course.id"))
+    course_id = db.Column(db.String, db.ForeignKey("course.id"))
+    prequisite_id = db.Column(db.String, db.ForeignKey("course.id"))
 
 #endpoint REGISTRASI & UPDATE PENGGUNA
 @app.route('/user/regis', methods=['POST'])
@@ -89,7 +87,7 @@ def create_course():
     data=request.get_json()
     new_course = Course(
         id = data.get('id'),
-        Nama = data.get('Nama'),
+        nama = data.get('nama'),
         deskripsi = data.get('deskripsi'),
         kategori = data.get('kategori')
     )
@@ -110,7 +108,22 @@ def update_course(id):
     db.session.commit()
     return {"message": "Hore! Anda berhasil mengupdate data."}
 
-#
+#Endpoint Enroll course
+@app.route('/course/enroll/<id>', methods=['POST'])
+def enroll_course(id):
+    user = login()
+    print(user.id)
+    if user.role == 'Student':
+        add_enroll = Coursedata(user_id = user.id, course_id = id, status = "in progress")
+        
+        db.session.add(add_enroll)
+        db.session.commit()
+        return {"message": "Hore! Anda berhasil."}
+
+#Endpoint enroll complete
+# @app.route('/course/enroll/<id>', methods=['PUT'])
+# def complete_course(id):
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
